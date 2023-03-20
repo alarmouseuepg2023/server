737298fd5f25fb9bfc48781a5dceba95c85e6adf
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-03-20 15:51:36 -0300

    feat(providers): unique identifier provider - completed

--- src/infra/containers/providers.container.ts
+++ src/infra/containers/providers.container.ts
@@ -3,6 +3,10 @@ import { container } from "tsyringe";
 import { AuthTokenProvider, IAuthTokenProvider } from "@providers/authToken";
 import { DateProvider, IDateProvider } from "@providers/date";
 import { HashProvider, IHashProvider } from "@providers/hash";
+import {
+  IUniqueIdentifierProvider,
+  UniqueIdentifierProvider,
+} from "@providers/uniqueIdentifier";
 import { IValidatorsProvider, ValidatorsProvider } from "@providers/validators";
 
 container.registerSingleton<IHashProvider>("HashProvider", HashProvider);
@@ -17,4 +21,9 @@ container.registerSingleton<IDateProvider>("DateProvider", DateProvider);
 container.registerSingleton<IValidatorsProvider>(
   "ValidatorsProvider",
   ValidatorsProvider
+);
+
+container.registerSingleton<IUniqueIdentifierProvider>(
+  "UniqueIdentifierProvider",
+  UniqueIdentifierProvider
 );
