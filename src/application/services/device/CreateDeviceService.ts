4f9afcc1a8470b4799e509d1cb9eb7fa9a57940e
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-03-21 19:33:22 -0300

    feat(services): create device - initial validations

--- src/application/services/device/CreateDeviceService.ts
+++ src/application/services/device/CreateDeviceService.ts
@@ -1,11 +1,73 @@
+import i18n from "i18n";
+import { inject, injectable } from "tsyringe";
+
+import { VarcharMaxLength } from "@commons/VarcharMaxLength";
+import { AppError } from "@handlers/error/AppError";
+import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
 import { CreateDeviceRequestModel } from "@http/dtos/device/CreateDeviceRequestModel";
 import { CreateDeviceResponseModel } from "@http/dtos/device/CreateDeviceResponseModel";
+import { IValidatorsProvider } from "@providers/validators";
 
+@injectable()
 class CreateDeviceService {
-  public async execute(
-    obj: CreateDeviceRequestModel
-  ): Promise<CreateDeviceResponseModel> {
-    console.log(obj);
+  constructor(
+    @inject("ValidatorsProvider")
+    private validatorsProvider: IValidatorsProvider
+  ) {}
+
+  public async execute({
+    macAddress,
+    nickname,
+    ownerPassword,
+    wifiPassword,
+    wifiSsid,
+  }: CreateDeviceRequestModel): Promise<CreateDeviceResponseModel> {
+    if (stringIsNullOrEmpty(nickname))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorNicknameRequired"));
+
+    if (
+      !this.validatorsProvider.length(
+        nickname,
+        VarcharMaxLength.DEVICE_NICKNAME
+      )
+    )
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__mf("ErrorVarCharMaxLengthExceeded", [
+          i18n.__("RandomWord_NickName"),
+          VarcharMaxLength.DEVICE_NICKNAME,
+        ])
+      );
+
+    if (stringIsNullOrEmpty(macAddress))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorMacAddressRequired"));
+
+    if (!this.validatorsProvider.macAddress(macAddress))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorMacAddressInvalid"));
+
+    if (stringIsNullOrEmpty(wifiSsid))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorWifiSsidRequired"));
+
+    if (
+      !this.validatorsProvider.length(
+        wifiSsid,
+        VarcharMaxLength.DEVICE_WIFI_SSID
+      )
+    )
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__mf("ErrorVarCharMaxLengthExceeded", [
+          i18n.__("RandomWord_WifiSsid"),
+          VarcharMaxLength.DEVICE_WIFI_SSID,
+        ])
+      );
+
+    if (stringIsNullOrEmpty(wifiPassword))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorWifiPasswordRequired"));
+
+    if (stringIsNullOrEmpty(ownerPassword))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorOwnerPasswordRequired"));
+
     return {} as any;
   }
 }
--- src/common/VarcharMaxLength.ts
+++ src/common/VarcharMaxLength.ts
@@ -2,6 +2,10 @@ class VarcharMaxLength {
   public static readonly USER_EMAIL: number = 100;
 
   public static readonly USER_NAME: number = 64;
+
+  public static readonly DEVICE_NICKNAME: number = 32;
+
+  public static readonly DEVICE_WIFI_SSID: number = 32;
 }
 
 export { VarcharMaxLength };
