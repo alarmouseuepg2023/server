4fa7a8826f8e130eba97e1d51e052376c5f0c04b
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-03-22 15:29:40 -0300

    feat(services): create invite - initial validations

--- src/application/services/invite/CreateInviteService.ts
+++ src/application/services/invite/CreateInviteService.ts
@@ -1,11 +1,33 @@
-import { CreateInviteRequestModel } from "@http/dtos/invite/CreateInviteRequestModel";
+import i18n from "i18n";
+import { inject, injectable } from "tsyringe";
 
+import { AppError } from "@handlers/error/AppError";
+import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
+import { CreateInviteRequestModel } from "@http/dtos/invite/CreateInviteRequestModel";
+import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
+
+@injectable()
 class CreateInviteService {
+  constructor(
+    @inject("UniqueIdentifierProvider")
+    private uniqueIdentifierProvider: IUniqueIdentifierProvider
+  ) {}
+
   public async execute({
     guestId,
     ownerId,
   }: CreateInviteRequestModel): Promise<void> {
-    console.log(guestId, ownerId);
+    if (stringIsNullOrEmpty(guestId))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorGuestIdRequired"));
+
+    if (stringIsNullOrEmpty(ownerId))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorOwnerIdRequired"));
+
+    if (
+      !this.uniqueIdentifierProvider.isValid(guestId) ||
+      !this.uniqueIdentifierProvider.isValid(ownerId)
+    )
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorUUIDInvalid"));
   }
 }
 
