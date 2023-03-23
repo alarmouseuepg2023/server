d2b6590b5f773d85e408343c148ac0fcb1e8cc3e
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-03-23 09:25:29 -0300

    fix(providers): mask provider - fix timestamp format

--- src/providers/mask/implementations/MaskProvider.ts
+++ src/providers/mask/implementations/MaskProvider.ts
@@ -15,7 +15,7 @@ class MaskProvider implements IMaskProvider {
       .toISOString()
       .replace(
         /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}).*$/,
-        "$1/$2/$3 às $4:$5"
+        "$3/$2/$1 às $4:$5"
       );
 }
 
