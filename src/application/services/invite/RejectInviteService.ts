7710979ff0644919b8b2d16f26adb4b5a3bb85cf
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-03-27 11:37:53 -0300

    feat(services): reject invite - completed

---
+++ src/application/services/invite/RejectInviteService.ts
@@ -0,0 +1,50 @@
+import { inject, injectable } from "tsyringe";
+
+import { InviteStatusDomain } from "@domains/InviteStatusDomain";
+import { IDeviceAccessControlRepository } from "@infra/database/repositories/deviceAccessControl";
+import { IInviteRepository } from "@infra/database/repositories/invite";
+import { IUserRepository } from "@infra/database/repositories/user";
+import { IDateProvider } from "@providers/date";
+import { IHashProvider } from "@providers/hash";
+import { IMaskProvider } from "@providers/mask";
+import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
+import { IValidatorsProvider } from "@providers/validators";
+
+import { AnswerInviteService } from "./AnswerInviteService";
+
+@injectable()
+class RejectInviteService extends AnswerInviteService {
+  constructor(
+    @inject("UniqueIdentifierProvider")
+    uniqueIdentifierProvider: IUniqueIdentifierProvider,
+    @inject("UserRepository")
+    userRepository: IUserRepository,
+    @inject("InviteRepository")
+    inviteRepository: IInviteRepository,
+    @inject("HashProvider")
+    hashProvider: IHashProvider,
+    @inject("DateProvider")
+    dateProvider: IDateProvider,
+    @inject("DeviceAccessControlRepository")
+    deviceAccessControl: IDeviceAccessControlRepository,
+    @inject("ValidatorsProvider")
+    validatorsProvider: IValidatorsProvider,
+    @inject("MaskProvider")
+    maskProvider: IMaskProvider
+  ) {
+    super(
+      uniqueIdentifierProvider,
+      userRepository,
+      inviteRepository,
+      hashProvider,
+      dateProvider,
+      deviceAccessControl,
+      validatorsProvider,
+      maskProvider
+    );
+  }
+
+  protected getInviteStatus = (): number => InviteStatusDomain.REJECTED;
+}
+
+export { RejectInviteService };
--- src/application/services/invite/index.ts
+++ src/application/services/invite/index.ts
@@ -2,8 +2,10 @@ import { AcceptInviteService } from "./AcceptInviteService";
 import { AnswerInviteService } from "./AnswerInviteService";
 import { CreateInviteService } from "./CreateInviteService";
 import { ListInvitsService } from "./ListInvitsService";
+import { RejectInviteService } from "./RejectInviteService";
 
 export {
+  RejectInviteService,
   AcceptInviteService,
   ListInvitsService,
   AnswerInviteService,
