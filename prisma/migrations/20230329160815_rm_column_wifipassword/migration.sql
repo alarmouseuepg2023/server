4cbb2a7356f9c5567f46c09f0392d88adda9c29a
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-03-29 13:22:28 -0300

    chore(db/schema): drop column wifiPassword at device

---
+++ prisma/migrations/20230329160815_rm_column_wifipassword/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `wifi_password` on the `device` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "device" DROP COLUMN "wifi_password";
