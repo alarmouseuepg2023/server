b5327338a6fb9660fcb7cbe77eb5f0de7e1cf7f7
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-05-17 09:35:50 -0300

    fix(middlewares): rbac - validate device id

--- src/infra/http/middlewares/RBACMiddleware.ts
+++ src/infra/http/middlewares/RBACMiddleware.ts
@@ -4,6 +4,7 @@ import { inject, injectable } from "inversify";
 import { AppError } from "@handlers/error/AppError";
 import { IMiddleware } from "@http/models/IMiddleware";
 import { HttpStatus } from "@http/utils/HttpStatus";
+import { IDeviceRepository } from "@infra/database/repositories/device";
 import { transaction } from "@infra/database/transaction";
 import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
 import { IDeviceAccessControlRepository } from "@repositories/deviceAccessControl";
@@ -13,6 +14,8 @@ class RBACMiddleware {
   constructor(
     @inject("DeviceAccessControlRepository")
     private deviceAccessControlRepository: IDeviceAccessControlRepository,
+    @inject("DeviceRepository")
+    private deviceRepository: IDeviceRepository,
     @inject("UniqueIdentifierProvider")
     private uniqueIdentifierProvider: IUniqueIdentifierProvider
   ) {}
@@ -26,13 +29,17 @@ class RBACMiddleware {
       if (!this.uniqueIdentifierProvider.isValid(deviceId))
         throw new AppError("BAD_REQUEST", i18n.__("ErrorUUIDInvalid"));
 
-      const [hasRole] = await transaction([
+      const [hasDevice, hasRole] = await transaction([
+        this.deviceRepository.getById({ deviceId }),
         this.deviceAccessControlRepository.verifyRole({
           role,
           userId: id,
           deviceId,
         }),
       ]);
+
+      if (!hasDevice)
+        throw new AppError("BAD_REQUEST", i18n.__("ErrorDeviceNotFound"));
 
       if (!hasRole)
         return res.status(HttpStatus.UNAUTHORIZED).json({
