ca6727d17dabbaa3d764017cd0663ffd7d30786b
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-05-24 09:49:34 -0300

    feat(infra): list alarm events - filters initial setup

--- src/infra/dtos/alarmEvents/ListAlarmEventsRequestModel.ts
+++ src/infra/dtos/alarmEvents/ListAlarmEventsRequestModel.ts
@@ -1,6 +1,12 @@
 import { IPaginationOptions } from "@http/models/IPaginationOptions";
 
-type ListAlarmEventsRequestModel = IPaginationOptions & {
+type ListAlarmEventsRequestModel = IPaginationOptions<{
+  status?: string;
+  date?: {
+    start?: string;
+    end?: string;
+  };
+}> & {
   deviceId: string;
 };
 
