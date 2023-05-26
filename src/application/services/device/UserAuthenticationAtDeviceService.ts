a97fa7df7f735350ae4c37f765b2ae036e4f2ec7
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-05-26 15:38:52 -0300

    feat(services): user authentication at device - completed

--- src/application/services/device/UserAuthenticationAtDeviceService.ts
+++ src/application/services/device/UserAuthenticationAtDeviceService.ts
@@ -1,13 +1,57 @@
-import { injectable } from "inversify";
+import i18n from "i18n";
+import { inject, injectable } from "inversify";
 
+import { AppError } from "@handlers/error/AppError";
+import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
+import { IDeviceAccessControlRepository } from "@infra/database/repositories/deviceAccessControl";
+import { transaction } from "@infra/database/transaction";
 import { UserAuthenticationAtDeviceRequestModel } from "@infra/dtos/device/UserAuthenticationAtDeviceRequestModel";
+import { IHashProvider } from "@providers/hash";
+import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
 
 @injectable()
 class UserAuthenticationAtDeviceService {
-  public async execute(
-    obj: UserAuthenticationAtDeviceRequestModel
-  ): Promise<void> {
-    console.log(obj);
+  constructor(
+    @inject("UniqueIdentifierProvider")
+    private uniqueIdentifierProvider: IUniqueIdentifierProvider,
+    @inject("HashProvider")
+    private hashProvider: IHashProvider,
+    @inject("DeviceAccessControlRepository")
+    private deviceAccessControlRepository: IDeviceAccessControlRepository
+  ) {}
+
+  public async execute({
+    deviceId,
+    password,
+    userId,
+  }: UserAuthenticationAtDeviceRequestModel): Promise<void> {
+    if (stringIsNullOrEmpty(password))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorPasswordRequired"));
+
+    if (stringIsNullOrEmpty(userId))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorUserIdRequired"));
+
+    if (stringIsNullOrEmpty(deviceId))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorDeviceIdRequired"));
+
+    if (
+      !this.uniqueIdentifierProvider.isValid(userId) ||
+      !this.uniqueIdentifierProvider.isValid(deviceId)
+    )
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorUUIDInvalid"));
+
+    const [hasDevice] = await transaction([
+      this.deviceAccessControlRepository.getById({ deviceId, userId }),
+    ]);
+
+    if (!hasDevice)
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorDeviceNotFound"));
+
+    if (!(await this.hashProvider.compare(password, hasDevice.password)))
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__("ErrorResetPasswdOldPasswordInvalid")
+      );
   }
 }
 
