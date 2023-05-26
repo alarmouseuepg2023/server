e7d185035717cef90503d46d48445f7683c2dcf0
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-05-26 15:50:00 -0300

    feat(services): user authentication at device - device access control handling

--- src/application/services/device/UserAuthenticationAtDeviceService.ts
+++ src/application/services/device/UserAuthenticationAtDeviceService.ts
@@ -1,11 +1,13 @@
 import i18n from "i18n";
 import { inject, injectable } from "inversify";
 
+import { ConstantsKeys } from "@commons/ConstantsKeys";
 import { AppError } from "@handlers/error/AppError";
 import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
 import { IDeviceAccessControlRepository } from "@infra/database/repositories/deviceAccessControl";
 import { transaction } from "@infra/database/transaction";
 import { UserAuthenticationAtDeviceRequestModel } from "@infra/dtos/device/UserAuthenticationAtDeviceRequestModel";
+import { IDateProvider } from "@providers/date";
 import { IHashProvider } from "@providers/hash";
 import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
 
@@ -17,7 +19,9 @@ class UserAuthenticationAtDeviceService {
     @inject("HashProvider")
     private hashProvider: IHashProvider,
     @inject("DeviceAccessControlRepository")
-    private deviceAccessControlRepository: IDeviceAccessControlRepository
+    private deviceAccessControlRepository: IDeviceAccessControlRepository,
+    @inject("DateProvider")
+    private dateProvider: IDateProvider
   ) {}
 
   public async execute({
@@ -40,18 +44,77 @@ class UserAuthenticationAtDeviceService {
     )
       throw new AppError("BAD_REQUEST", i18n.__("ErrorUUIDInvalid"));
 
-    const [hasDevice] = await transaction([
+    const [hasDeviceAccessControl] = await transaction([
       this.deviceAccessControlRepository.getById({ deviceId, userId }),
     ]);
 
-    if (!hasDevice)
+    if (!hasDeviceAccessControl)
       throw new AppError("BAD_REQUEST", i18n.__("ErrorDeviceNotFound"));
 
-    if (!(await this.hashProvider.compare(password, hasDevice.password)))
-      throw new AppError(
-        "BAD_REQUEST",
-        i18n.__("ErrorResetPasswdOldPasswordInvalid")
-      );
+    if (hasDeviceAccessControl.blocked)
+      throw new AppError("UNAUTHORIZED", i18n.__("ErrorUserIsBlockedAtDevice"));
+
+    const now = this.dateProvider.now();
+
+    if (
+      !(await this.hashProvider.compare(
+        password || "",
+        hasDeviceAccessControl.password
+      ))
+    ) {
+      const attempts =
+        !hasDeviceAccessControl.lastFailedUnlock ||
+        this.dateProvider.isBefore(
+          now,
+          this.dateProvider.addMinutes(
+            hasDeviceAccessControl.lastFailedUnlock,
+            ConstantsKeys.MINUTES_TO_RESET_FAILED_LOGIN_ATTEMPTS_AT_DEVICE
+          )
+        )
+          ? hasDeviceAccessControl.unlockAttempts + 1
+          : 1;
+
+      const [deviceAccessControlUpdated] = await transaction([
+        this.deviceAccessControlRepository.updateControlProps({
+          deviceId,
+          userId,
+          unlockAttempts: attempts,
+          lastFailedUnlock: now,
+          blocked: attempts === ConstantsKeys.MAX_LOGIN_ATTEMPTS_AT_DEVICE,
+        }),
+      ]);
+
+      if (
+        deviceAccessControlUpdated.unlockAttempts <
+        ConstantsKeys.MAX_LOGIN_ATTEMPTS_AT_DEVICE
+      )
+        throw new AppError(
+          "UNAUTHORIZED",
+          i18n.__mf(
+            "ErrorLoginAtDeviceUserUnauthorizedAndWillBeBlockedInFewAttempts",
+            [
+              ConstantsKeys.MAX_LOGIN_ATTEMPTS_AT_DEVICE -
+                deviceAccessControlUpdated.unlockAttempts,
+            ]
+          )
+        );
+
+      if (deviceAccessControlUpdated.blocked)
+        throw new AppError(
+          "UNAUTHORIZED",
+          i18n.__("ErrorLoginAtDeviceUserWillBeBlocked")
+        );
+    }
+
+    await transaction([
+      this.deviceAccessControlRepository.updateControlProps({
+        deviceId,
+        userId,
+        unlockAttempts: 0,
+        blocked: false,
+        lastFailedUnlock: null,
+      }),
+    ]);
   }
 }
 
