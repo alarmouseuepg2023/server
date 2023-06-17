9a294eca6c42fc6bd58c0503fa20b5954bce2023
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-17 15:13:59 -0300

    feat(routes): push notifications - initial setup

--- src/infra/http/routes/index.routes.ts
+++ src/infra/http/routes/index.routes.ts
@@ -7,6 +7,7 @@ import { routes as authRoutes } from "./auth.routes";
 import { routes as deviceRoutes } from "./device.routes";
 import { routes as guestRoutes } from "./guest.routes";
 import { routes as inviteRoutes } from "./invite.routes";
+import { routes as pushNotificationsRoutes } from "./pushNotifications.routes";
 import { routes as userRoutes } from "./user.routes";
 
 const routes = Router();
@@ -17,5 +18,6 @@ routes.use(RoutesPrefix.USER, userRoutes);
 routes.use(RoutesPrefix.DEVICE, deviceRoutes);
 routes.use(RoutesPrefix.INVITE, inviteRoutes);
 routes.use(RoutesPrefix.ALARM_EVENTS, alarmEventsRoutes);
+routes.use(RoutesPrefix.PUSH_NOTIFICATIONS, pushNotificationsRoutes);
 
 export { routes };
