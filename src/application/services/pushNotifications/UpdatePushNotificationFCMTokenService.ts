d2efd0aba8ed0a0a78d97acff00c8af656757295
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-17 18:05:27 -0300

    feat(services): update push notification token - completed

--- src/application/services/pushNotifications/UpdatePushNotificationFCMTokenService.ts
+++ src/application/services/pushNotifications/UpdatePushNotificationFCMTokenService.ts
@@ -1,19 +1,68 @@
-import { injectable } from "inversify";
+import i18n from "i18n";
+import { inject, injectable } from "inversify";
 
+import { VarcharMaxLength } from "@commons/VarcharMaxLength";
+import { AppError } from "@handlers/error/AppError";
+import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
+import { IPushNotificationsRepository } from "@infra/database/repositories/pushNotification";
+import { transaction } from "@infra/database/transaction";
 import { UpdatePushNotificationFCMTokenRequestModel } from "@infra/dtos/pushNotifications/UpdatePushNotificationFCMTokenRequestModel";
 import { UpdatePushNotificationFCMTokenResponseModel } from "@infra/dtos/pushNotifications/UpdatePushNotificationFCMTokenResponseModel";
+import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
+import { IValidatorsProvider } from "@providers/validators";
 
 @injectable()
 class UpdatePushNotificationFCMTokenService {
-  public async execute(
-    obj: UpdatePushNotificationFCMTokenRequestModel
-  ): Promise<UpdatePushNotificationFCMTokenResponseModel> {
-    console.log(obj);
+  constructor(
+    @inject("UniqueIdentifierProvider")
+    private uniqueIdentifierProvider: IUniqueIdentifierProvider,
+    @inject("PushNotificationsRepository")
+    private pushNotificationsRepository: IPushNotificationsRepository,
+    @inject("ValidatorsProvider")
+    private validatorsProvider: IValidatorsProvider
+  ) {}
+
+  public async execute({
+    token,
+    userId,
+  }: UpdatePushNotificationFCMTokenRequestModel): Promise<UpdatePushNotificationFCMTokenResponseModel> {
+    if (stringIsNullOrEmpty(userId))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorUserIdRequired"));
+
+    if (stringIsNullOrEmpty(token))
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__("ErrorPushNotificationFCMTokenRequired")
+      );
+
+    if (
+      !this.validatorsProvider.length(
+        token,
+        VarcharMaxLength.PUSH_NOTIFICATIONS_TOKEN
+      )
+    )
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__mf("ErrorVarCharMaxLengthExceeded", [
+          i18n.__("RandomWord_FCMToken"),
+          VarcharMaxLength.PUSH_NOTIFICATIONS_TOKEN,
+        ])
+      );
+
+    if (!this.uniqueIdentifierProvider.isValid(userId))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorUUIDInvalid"));
+
+    const [updated] = await transaction([
+      this.pushNotificationsRepository.save({
+        fcmToken: token,
+        userId,
+        notificationEnabled: true,
+      }),
+    ]);
 
     return {
-      fcmToken: "",
-      notificationEnabled: true,
-      userId: "",
+      ...updated,
+      userId,
     };
   }
 }
--- src/common/VarcharMaxLength.ts
+++ src/common/VarcharMaxLength.ts
@@ -6,6 +6,8 @@ class VarcharMaxLength {
   public static readonly DEVICE_NICKNAME: number = 32;
 
   public static readonly DEVICE_WIFI_SSID: number = 32;
+
+  public static readonly PUSH_NOTIFICATIONS_TOKEN: number = 255;
 }
 
 export { VarcharMaxLength };
