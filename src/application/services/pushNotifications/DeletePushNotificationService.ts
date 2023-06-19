8ec3b919fb7c17c4cc35a841a9802cf3d6ab495e
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-19 10:54:01 -0300

    feat(services): delete push notification - completed

--- src/application/services/pushNotifications/DeletePushNotificationService.ts
+++ src/application/services/pushNotifications/DeletePushNotificationService.ts
@@ -1,14 +1,46 @@
-import { injectable } from "inversify";
+import i18n from "i18n";
+import { inject, injectable } from "inversify";
 
+import { AppError } from "@handlers/error/AppError";
+import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
+import { IPushNotificationsRepository } from "@infra/database/repositories/pushNotification";
+import { transaction } from "@infra/database/transaction";
 import { DeletePushNotificationRequestModel } from "@infra/dtos/pushNotifications/DeletePushNotificationRequestModel";
+import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
 
 @injectable()
 class DeletePushNotificationService {
-  public async execute(
-    obj: DeletePushNotificationRequestModel
-  ): Promise<boolean> {
-    console.log(obj);
-    return true;
+  constructor(
+    @inject("UniqueIdentifierProvider")
+    private uniqueIdentifierProvider: IUniqueIdentifierProvider,
+    @inject("PushNotificationsRepository")
+    private pushNotificationRepository: IPushNotificationsRepository
+  ) {}
+
+  public async execute({
+    userId,
+  }: DeletePushNotificationRequestModel): Promise<boolean> {
+    if (stringIsNullOrEmpty(userId))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorUserIdRequired"));
+
+    if (!this.uniqueIdentifierProvider.isValid(userId))
+      throw new AppError("BAD_REQUEST", i18n.__("ErrorUUIDInvalid"));
+
+    const [hasPushNotification] = await transaction([
+      this.pushNotificationRepository.getById({ userId }),
+    ]);
+
+    if (!hasPushNotification)
+      throw new AppError(
+        "BAD_REQUEST",
+        i18n.__("ErrorPushNotificationNotFound")
+      );
+
+    const [deleted] = await transaction([
+      this.pushNotificationRepository.delete({ userId }),
+    ]);
+
+    return !!deleted;
   }
 }
 
