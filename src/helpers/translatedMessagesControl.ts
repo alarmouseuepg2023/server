601c28f747648954ae044dfcb89d2633fbbd990a
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-20 19:47:31 -0300

    feat(helpers): translated messages control - completed

---
+++ src/helpers/translatedMessagesControl.ts
@@ -0,0 +1,8 @@
+import i18n from "i18n";
+
+const getMessage = (key: string): string => i18n.__(key);
+
+const getVariableMessage = (key: string, ...replace: any[]): string =>
+  i18n.__mf(key, replace);
+
+export { getMessage, getVariableMessage };
