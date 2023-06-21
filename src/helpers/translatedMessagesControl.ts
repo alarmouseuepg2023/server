0525cba360369881f996320b2a87c9c74633463d
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-20 21:04:31 -0300

    fix(helpers): translated messagens control - fix get variable method

--- src/helpers/translatedMessagesControl.ts
+++ src/helpers/translatedMessagesControl.ts
@@ -2,7 +2,7 @@ import i18n from "i18n";
 
 const getMessage = (key: string): string => i18n.__(key);
 
-const getVariableMessage = (key: string, ...replace: any[]): string =>
+const getVariableMessage = (key: string, replace: any[]): string =>
   i18n.__mf(key, replace);
 
 export { getMessage, getVariableMessage };
